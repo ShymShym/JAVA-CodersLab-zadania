5a9f87e6b81d143aee9fa84e0dfdf602656e86ec
ShymShym <marta.b.szymczyk@gmail.com>
2019-08-28 00:10:18 +0200

    To jest brakujące zadanie Main6.java z folderu a.

---
+++ Main6.java
@@ -0,0 +1,13 @@
+/**Ten program ma wypisać dane autora:
+ * imię, nazwisko
+ * i login Github*/
+public class Main6 {
+    public static void main(String[] args) {
+        //imię
+        System.out.print("Marta ");
+        //nazwisko
+        System.out.println("Szymczyk");
+        //login GitHub
+        System.out.println("ShymShym");
+    }
+}
