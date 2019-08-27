e70a10c8ba9ac6aee503e8a149e6d468ceacfc96
Marta Szymczyk <marta.b.szymczyk@gmail.com>
2019-08-28 01:50:30 +0200

    To są zadania z drugiegoo folderu (b).

---
+++ b_Zmienne_i_typy_danych/Main1.java
@@ -0,0 +1,25 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		/*Ta zmienna przechowuje typ byte
+		byte byte9 = 23;
+		System.out.println(byte9);*/
+		//Ta zmienna przechowuje typ int
+		int int1 = 13;
+		System.out.println(int1);
+		//Ta zmienna przechowuje typ char
+		char char1 = 'M';
+		System.out.println(char1);
+		//Ta zmienna przechowuje typ long
+		long longOne = 2147483646;
+		System.out.println(longOne);
+		//Ta zmienna przechowuje typ double
+		double doubleTrouble = 1.1234567898765;
+		System.out.println(doubleTrouble);
+		//Ta zmienna przechowuje typ boolean
+		boolean boolT = true;
+		System.out.print(boolT);
+	}
+
+}
---
+++ b_Zmienne_i_typy_danych/Main3.java
@@ -0,0 +1,14 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		int nr1 = 2;
+		double nr2 = 3.141592;
+		double result = 0;
+		result = nr1+nr2;
+		System.out.println(result);
+		/*Zmienna "result" powinna być typu double,
+		ponieważ jedna ze zmiennych jest liczbą zmiennoprzecinkową typu double.*/
+	}
+
+}
