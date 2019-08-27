ab1cb5ee2e7cb3f50bc22e6f8cfa77eaa339cbfe
Marta Szymczyk <marta.b.szymczyk@gmail.com>
2019-08-27 23:01:21 +0200

    To są zadania z pierwszego folderu (a).

---
+++ a_Pierwszy_Program/Main3.java
@@ -0,0 +1,14 @@
+/** Komentarz dokumentacyjny.
+ * Na podstawie komentarzy dokumentacyjnych
+ * może zostać wygenerowana
+ * dokumentacja techniczna aplikacji. */
+public class Main3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pierwszy program JAVA");
+/*		System.out.println("Niepotrzebna linijka");
+		System.out.println("Niepotrzebna linijka");
+Tego typu komentarza można użyć na wielu linijkach równocześnie*/
+	}
+// A to jest komentarz jednolinijkowy
+}
---
+++ a_Pierwszy_Program/Main5.java
@@ -0,0 +1,11 @@
+
+public class Main5 {
+	//Poprawka: public class Main5. Było: "CodersLab".
+	public static void main(String[] args) {
+		System.out.print("Dzisiaj zaczynam kurs");
+		System.out.print("języka JAVA");
+		System.out.print("w CodersLab");
+
+	}
+
+}
